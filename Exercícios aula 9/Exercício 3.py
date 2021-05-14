f87acb87d0f9d45fe9d5d63a47d7d3a92fb069e2
Lucas <lucas.castorino@gmail.com>
2021-05-13 23:21:00 -0300

    Exercícios resolvidos até 13/05/2021

--- Exercícios aula 9/Exercício 3.py
+++ Exercícios aula 9/Exercício 3.py
@@ -1,5 +1,5 @@
 valor_pao = float(input("Digite o valor do pão").replace(',', '.'))
 print("Preço do pão: R$", valor_pao)
 print("Panificadora Pão de Ontem - Tabela de preços")
-for i in range(1, 11):
+for i in range(1, 51):
     print("{} - R$ {:.2f}".format(i, valor_pao*i))
